175ae6718ebe615eb5ac971f3f460270ed39fee2
William Harrison <william@williamdavidharrison.com.au>
2022-10-24 14:44:23 +0800

    Create house_with_garage.py

---
+++ house_with_garage.py
@@ -0,0 +1,26 @@
+from turtle import *
+
+speed(0)
+
+for i in range(5):
+  left(90)
+  forward(50)
+
+left(45)
+forward(35)
+left(90)
+forward(35)
+
+penup()
+goto(0, 0)
+left(135)
+pendown()
+
+for i in range(6):
+  forward(25)
+  left(90)
+
+right(45)
+forward(18)
+left(90)
+forward(18)
