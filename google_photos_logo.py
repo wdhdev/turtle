e286b74b345b529e7e8f0a57a6c2f211db16cc15
William Harrison <william@williamdavidharrison.com.au>
2022-11-03 09:43:48 +0800

    Add colour

--- google_photos_logo.py
+++ google_photos_logo.py
@@ -2,10 +2,64 @@ from turtle import *
 
 speed(0)
 
-for logo in range(4):
-  for i in range(180):
-    forward(1)
-    left(1)
+pensize(5)
 
-  left(90)
-  forward(115)
+pencolor("#DA483B")
+fillcolor("#DA483B")
+
+begin_fill()
+for i in range(180):
+  forward(1)
+  left(1)
+end_fill()
+
+left(90)
+forward(115)
+
+penup()
+forward(4)
+pendown()
+
+pencolor("#4486F4")
+fillcolor("#4486F4")
+
+begin_fill()
+for i in range(180):
+  forward(1)
+  left(1)
+end_fill()
+
+left(90)
+forward(115)
+
+penup()
+forward(4)
+pendown()
+
+pencolor("#1CA45C")
+fillcolor("#1CA45C")
+
+begin_fill()
+for i in range(180):
+  forward(1)
+  left(1)
+end_fill()
+
+left(90)
+forward(115)
+
+penup()
+forward(4)
+pendown()
+
+pencolor("#FFC718")
+fillcolor("#FFC718")
+
+begin_fill()
+for i in range(180):
+  forward(1)
+  left(1)
+end_fill()
+
+left(90)
+forward(115)
