01cc3c5e527a7443da1e6ce567b705dec58ef1ed
William Harrison <william@williamdavidharrison.com.au>
2022-11-03 09:46:58 +0800

    Create three_houses.py

---
+++ three_houses.py
@@ -0,0 +1,26 @@
+from turtle import *
+
+speed(0)
+
+pensize(4)
+
+pencolor("red")
+fillcolor("skyblue")
+
+for i in range(3):
+  begin_fill()
+  forward(30)
+  left(90)
+  forward(30)
+  left(45)
+  forward(30)
+  left(90)
+  forward(30)
+  left(45)
+  forward(30)
+  left(90)
+  forward(30)
+  end_fill()
+  penup()
+  forward(28)
+  pendown()
